06dfefa782a158586d17228a77eeb82ad441fc5b
AlexxxFlexxx <alexandre.peltier@ecole-hexagone.com>
2025-01-22 09:17:14 +0100

    test

--- entrainement-final/src/pages/NotFound/NotFound.jsx
+++ entrainement-final/src/pages/NotFound/NotFound.jsx
@@ -5,7 +5,7 @@ export default function NotFound() {
   return (
     <div className="not-found">
       <h1>404</h1>
-      <h2>Page Non Trouvée</h2>
+      <h2>Page perdue au combat</h2>
       <p>Retourez sur le champ de bataille à la recherche d'indices...</p>
       <Link to="/" className="home-link">Retour à l'accueil</Link>
     </div>
