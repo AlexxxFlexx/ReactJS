1186b7e2fee82b98d06bb2179cbe1d485643bc42
AlexxxFlexxx <alexandre.peltier@ecole-hexagone.com>
2025-02-03 13:29:35 +0100

    C'est bon ?

--- Éval/Exam-final/src/pages/NotFound/NotFound.jsx
+++ Éval/Exam-final/src/pages/NotFound/NotFound.jsx
@@ -5,8 +5,8 @@ export default function NotFound() {
   return (
     <div className="not-found">
       <h1>404</h1>
-      <h2>Page perdue au combat...</h2>
-      <p>Trouvez un héro plus compétant</p>
+      <h2>Page introuvable.</h2>
+      <p>Trouvez un héro plus compétant !</p>
       <Link to="/" className="home-link">Retour à l'accueil</Link>
     </div>
   );
